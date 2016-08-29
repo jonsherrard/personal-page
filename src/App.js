7141b0be087c4ba961ea023a650c3cc461377cbf
Jon Sherrard <jon@andthats.it>
2016-08-29 18:17:41 +0100

    use functional components

--- src/App.js
+++ src/App.js
@@ -11,8 +11,10 @@ class App extends Component {
           title="Jonathon Sherrard."
           subtitle="Lead Developer & Tech Consultant."
         />
-        <section className="blue bg-washed-blue bt bw2 b--orange f3 fw5 ph3 ph3-ns ph4-m ph4-l pv6 mt0 db cf lh-title lh-copy-m">
-          <h2 className="lh-title f2 f-subheadline-l orange">A perfect software project:</h2>
+        <section className="bg-black-70 light-gray f3 fw5 ph3 ph3-ns ph4-m ph4-l pv6 mt0 db cf lh-title lh-copy-m">
+          <h2 className="lh-title f2 fw5 f-subheadline-l">
+            The perfect software project:
+          </h2>
           <Statement>
             Iterating towards simplicity.
           </Statement>
@@ -20,10 +22,10 @@ class App extends Component {
             Empowering developers.
           </Statement>
           <Statement>
-            Reliability over features.
+            Predictability, then features.
           </Statement>
           <Statement>
-            User testing, <em>and</em> intuition.
+            User testing, and intuition.
           </Statement>
         </section>
         <Footer />
--- src/Statement.js
+++ src/Statement.js
@@ -1,11 +1,8 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class Statement extends Component {
-  render() {
-    return (
-      <article className="w-100 w-50-m w-25-l dib">
-        <h3>{this.props.children}</h3>
-      </article>
-    )
-  }
-}
+const Statement = (props) =>
+  <article className="w-100 w-50-m w-25-l dib">
+    <h3 className="fw5">{props.children}</h3>
+  </article>
+
+export default Statement
